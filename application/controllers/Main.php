63d7a2741b0df9fb57af04c99bd08081309b3be2
AgustinoWhickey <wakembus98@gmail.com>
2019-10-08 20:33:55 -0700

    Remove comment

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -5,8 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 
 	public function index(){
+		
 		$this->load->view('template/header');
 		$this->load->view('login');
+		
 	}
 	
 	public function logout(){
