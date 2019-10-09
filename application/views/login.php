51910abfb51023c6e3608aa70eec3413ac26eaee
saifudin mahara <maharasaifudin@gmail.com>
2019-10-09 17:18:30 +0700

    function login static admin

--- application/views/login.php
+++ application/views/login.php
@@ -15,19 +15,19 @@
 
 					<div class="card-body">
 						<legend class="font-weight-semibold"><i class="icon-reading mr-2"></i> Sign in ke YBC SAP Portal</legend>
-						<form action="<?php echo base_url('/msi/dashboard');?>">
+						<form action="#" method="POST">
 							<div class="form-group">
 								<label>Name:</label>
-								<input type="text" class="form-control" placeholder="Eugene Kopyov">
+								<input type="text" class="form-control" id="name">
 							</div>
 
 							<div class="form-group">
 								<label>Password:</label>
-								<input type="password" class="form-control" placeholder="Your strong password">
+								<input type="password" class="form-control" id="password">
 							</div>
 
 							<div class="text-right">
-								<button type="submit" class="btn btn-primary">Submit form <i class="icon-paperplane ml-2"></i></button>
+								<button type="button" class="btn btn-primary" id="login">Submit form <i class="icon-user ml-2"></i></button>
 							</div>
 							<br>
 							<div class="text-right">
@@ -42,5 +42,29 @@
 
 		</div>
 		<!-- /vertical form options -->
+		
+		<script>
+			$(document).ready(function(){
+				base_url = 'http://localhost/msi-harvest/';
+				$("#login").click(function(){
+					name = $('#name').val().trim();
+					pass = $('#password').val().trim();
+
+					if(name=='' && pass==''){
+						swal("Oops...", "Name and password must not be blank", "error");
+					}else{
+						$.post(base_url+'login/userLogin',{Name: name, Password: pass},function(data){
+							if(data!=0){
+								window.location.replace(data);
+							}else{
+								swal("Oops...", "Something went wrong!", "error");
+							}
+						});
+					}
+				});
+			})
+			
+		
+		</script>
 </body>
 </html>
