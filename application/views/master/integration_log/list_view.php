ba47fc686ae4fbcb251fc4106a4e1755b72f4ad1
AgustinoWhickey <wakembus98@gmail.com>
2019-10-14 01:21:56 -0700

    Integration View

---
+++ application/views/master/integration_log/list_view.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php  $this->load->view("_template/head.php")?>
+	</head>
+	<body>
+	<?php  $this->load->view("_template/nav.php")?>
+		<div class="page-content">
+			<?php  $this->load->view("_template/sidebar.php")?>
+			<div class="content-wrapper">
+                <!-- <?php  $this->load->view("_template/breadcrumb.php")?> -->
+				<div class="content">
+                    <div class="card">
+                        <div class="card-header">
+                            <legend class="font-weight-semibold"><i class="icon-list mr-2"></i>List Manajemen Pengguna</legend>
+                        </div>
+                        <div class="card-body">
+                            <table id="table-manajemen" class="table table-striped " style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th style="text-align: left">*</th>
+                                        <th>Modul</th>
+                                        <th>Message</th>
+                                        <th>Error Time</th>
+                                        <th>Trans ID</th>
+                                        <th>Edit</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>                    
+				</div>
+				<?php  $this->load->view("_template/footer.php")?>
+			</div>
+		</div>
+        <?php  $this->load->view("_template/js.php")?>
+        <script>
+            $(document).ready(function(){
+                $('#table-manajemen').DataTable({
+                    "ordering":false,  "paging": true, "searching":true,
+                    "ajax": {
+                        "url":"<?php echo site_url('master/integration/showAllData');?>",
+                        "type":"POST"
+                    },
+                    "columns": [
+                        {"data":"no"},
+                        {"data":"modul"},
+                        {"data":"message"},
+                        {"data":"error_time"},
+                        {"data":"trans_id"},
+                        {"data":"no","className":"dt-center", render:function(data, type, row, meta){
+                                rr = `<a href='<?php echo site_url('master/integration/edit')?>' ><i class='icon-file-plus2' title="Edit"></i></a>&nbsp;`;
+                                return rr;
+                            }
+                        }
+                    ]
+                });
+
+                deleteConfirm = (url)=>{
+                    $('#btn-delete').attr('href', url);
+	                $('#deleteModal').modal();
+                }
+            });
+        
+        </script>
+	</body>
+</html>
