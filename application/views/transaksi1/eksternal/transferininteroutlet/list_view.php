c8a4c4fcf4e6495414aadcd7368621ccfbc00603
AgustinoWhickey <wakembus98@gmail.com>
2019-10-18 01:01:45 -0700

    Finishing Transfer In Inter Outler

--- application/views/transaksi1/eksternal/transferininteroutlet/list_view.php
+++ application/views/transaksi1/eksternal/transferininteroutlet/list_view.php
@@ -11,145 +11,155 @@
                 <!-- <?php  $this->load->view("_template/breadcrumb.php")?> -->
 				<div class="content">
                     <div class="card">
-                        <div class="card-body">
-                            <form action="#" method="POST">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <fieldset>
-                                            <legend class="font-weight-semibold"><i class="icon-reading mr-2"></i>Transfer Out Inter Outlet</legend>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label"><i>SR Number — Transfer In Inter Outlet</i></label>
-												<div class="col-lg-9">
-													<select class="form-control">
-														<option value="94897" selected>94897</option>
-													</select>
-													<a href="#">Pilih ulang SR Number</a>
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label">Transfer Out No</label>
-												<div class="col-lg-9"><b>200352</b>
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label">Transfer In Number</label>
-												<div class="col-lg-9">
-													<i>(Auto Number after Posting to SAP)</i>
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label">Outlet</label>
-												<div class="col-lg-9">
-													WDFGBNST - Bintaro
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label">Storage Transit Location</label>
-												<div class="col-lg-9">
-													WDFGBNST - Bintaro
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label">Delivery Outlet</label>
-												<div class="col-lg-9">
-													<b>WDFGAMPM - Ampera - PM</b>
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label">Delivery Date</label>
-												<div class="col-lg-9">
-													<b>16-10-2019</b>
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label">Status</label>
-												<div class="col-lg-9">
-													<b>Not Approved</b>
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<label class="col-lg-3 col-form-label">Posting Date</label>
-												<div class="col-lg-9">
-													<input type="text" class="form-control" value="17-10-2019">
-												</div>
-											</div>
-											
-											<div class="form-group row">
-												<div class="col-lg-12 text-right">
-													<div class="text-right">
-														<button type="submit" class="btn btn-primary">Save <i class="icon-pencil5 ml-2"></i></button>
-														<button type="submit" class="btn btn-success">Approve <i class="icon-paperplane ml-2"></i></button>
-													</div>
-												</div>
-                                            </div>
-											
-                                        </fieldset>
-                                    </div>
-								</div>	
-								<br>
-								
-								<table id="table-transferininteroutlet" class="table table-striped " style="width:100%">
-									<thead>
-										<tr>
-											<th style="text-align: left">*</th>
-											<th>Material No</th>
-											<th>Material Desc</th>
-											<th>Good Issue Qty</th>
-											<th>Actual GR. Qty</th>
-											<th>Uom</th>
-											<th>Val</th>
-											<th>Variance</th>
-										</tr>
-									</thead>
-									<tbody>
-									</tbody>
-								</table>
-                            </form>
-                        
+                        <div class="card-header">
+                            <legend class="font-weight-semibold"><i class="icon-search4 mr-2"></i>Search of Transfer Out Inter Outlet</legend>  
                         </div>
-                    </div>
+                        <div class="card-body">
+
+							<form action="#" method="POST">
+								<div class="row">
+									<div class="col-md-12">
+										<div class="form-group row">
+											<label class="col-lg-3 col-form-label">Dari Tanggal</label>
+											<div class="col-lg-3 input-group date">
+												<input type="text" class="form-control" id="fromDate">
+												<div class="input-group-prepend">
+													<span class="input-group-text" id="basic-addon1">
+														<i class="icon-calendar"></i>
+													</span>
+												</div>
+											</div>
+											<label class="col-lg-2 col-form-label">Sampai Tanggal</label>
+											<div class="col-lg-4 input-group date">
+												<input type="text" class="form-control" id="toDate">
+												<div class="input-group-prepend">
+													<span class="input-group-text" id="basic-addon1">
+														<i class="icon-calendar"></i>
+													</span>
+												</div>
+											</div>
+										</div>
+
+
+										<div class="form-group row">
+											<label class="col-lg-3 col-form-label">Status</label>
+											<div class="col-lg-9">
+												<select class="form-control form-control-select2" data-live-search="true">
+													<option value="">none selected</option>
+													<option value="approved">Approved</option>
+													<option value="notapproved">Not Approved</option>
+												</select>
+											</div>
+										</div>
+
+										<div class="text-right">
+											<button type="submit" class="btn btn-primary">Search<i class="icon-search4  ml-2"></i></button>
+										</div>
+									</div>
+								</div>
+							</form>
+                        </div>                        
+                    
+                        <div class="card-header">
+                            <a href="<?php echo site_url('transaksi1/transferininteroutlet/add') ?>" class="btn btn-primary"> Add New</a>
+                            <input type="button" value="Delete" class="btn btn-danger" id="deleteRecord">  
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12" style="overflow:auto">
+                                    <table id="tableWhole" class="table table-striped" style="width:100%" >
+                                        <thead>
+                                            <tr>
+                                                <th style="text-align: left"></th>
+                                                <th style="text-align: center">Action</th>
+                                                <th style="text-align: center">ID</th>
+                                                <th style="text-align: center">Transfer In No</th>
+                                                <th style="text-align: center">Store Room Request (SR) Number</th>
+                                                <th style="text-align: center">Transfer No Slip</th>
+                                                <th style="text-align: center">Delivery Outlet</th>
+                                                <th style="text-align: center">Delivery Date</th>
+                                                <th style="text-align: center">Posting Date</th>
+                                                <th style="text-align: center">Status</th>
+                                                <th style="text-align: center">User</th>
+                                                <th style="text-align: center">Approved by</th>
+                                                <th style="text-align: center">Last Modified</th>
+                                                <th style="text-align: center">Cancel</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        </div>                   
 				</div>
 				<?php  $this->load->view("_template/footer.php")?>
 			</div>
 		</div>
-		<?php  $this->load->view("_template/js.php")?>
-		<script>
+        <?php  $this->load->view("_template/modal_delete.php")?>
+        <?php  $this->load->view("_template/js.php")?>
+        <script>
             $(document).ready(function(){
-                $('#table-transferininteroutlet').DataTable({
+                $('#fromDate').datepicker();
+                $('#toDate').datepicker();
+                dataTable = $('#tableWhole').DataTable({
                     "ordering":false,  "paging": true, "searching":true,
                     "ajax": {
-                        "url":"<?php echo site_url('transaksi1/transferininteroutlet/showAllData');?>",
+                        "url":"<?php echo site_url('transaksi1/transferininteroutlet/showListData');?>",
                         "type":"POST"
                     },
                     "columns": [
-                        {"data":"no"},
-                        {"data":"material_no"},
-                        {"data":"material_desc"},
-                        {"data":"gi_qty"},
-						{"data":"gr_qty", "className":"dt-center", render:function(data, type, row, meta){
-                            rr=`<input type="text" class="form-control" value="${data}">`;
+                        {"data":"no", "className":"dt-center", render:function(data, type, row, meta){
+                            rr=`<input type="checkbox" class="check_delete" id="chk_${data}" value="${data}" onclick="checkcheckbox();">`;
                             return rr;
                         }},
-                        {"data":"uom"},
-						{"data":"val", "className":"dt-center", render:function(data, type, row, meta){
-                            rr=`<select class="form-control"><option value="variance" selected>Variance</option><option value="rmbeverage">RM Beverage</option></select>`;
-                            return rr;
+                        {"data":"action", "className":"dt-center", render:function(data, type, row, meta){
+                            rr = `<div style="width:100px">
+										<a href='#' ><i class='icon-file-excel' title="Excel"></i></a>&nbsp;
+										<a href='#' ><i class='icon-printer' title="Print"></i></a>&nbsp;
+                                        <a href='<?php echo site_url('transaksi1/transferininteroutlet/edit')?>' ><i class='icon-file-plus2' title="Edit"></i></a>&nbsp;
+                                    </div>`;
+                                        return rr;
                         }},
-						{"data":"variance", "className":"dt-center", render:function(data, type, row, meta){
-                            rr=`<input type="text" class="form-control" value="${data}">`;
-                            return rr;
-                        }},
+                        {"data":"id"},
+                        {"data":"tf_in_number"},
+                        {"data":"sr_req_number"},
+                        {"data":"tf_no_slip"},
+                        {"data":"delivery_outlet"},
+                        {"data":"delivery_date"},
+                        {"data":"posting_date"},
+                        {"data":"status"},
+                        {"data":"user"},
+                        {"data":"approved_by"},
+                        {"data":"last_modified"},
+                        {"data":"cancel"}
                     ]
                 });
+                $("#deleteRecord").click(function(){
+                    let deleteidArr=[];
+                    $("input:checkbox[class=check_delete]:checked").each(function(){
+                        deleteidArr.push($(this).val());
+                    })
+                    // mengecek ckeckbox tercheck atau tidak
+                    if(deleteidArr.length > 0){
+                        var confirmDelete = confirm("Do you really want to Delete records?");
+                        if(confirmDelete == true){
+                            $.ajax({
+                                url:"", //masukan url untuk delete
+                                type: "post",
+                                data:{deleteArr: deleteidArr},
+                                success:function(res) {
+                                    dataTable.ajax.reload();
+                                }
+                            });
+                        }
+                    }
+                });
+                deleteConfirm = (url)=>{
+                    $('#btn-delete').attr('href', url);
+	                $('#deleteModal').modal();
+                }
             });
         
         </script>
