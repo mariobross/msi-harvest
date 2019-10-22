2485355ced8ce773611e7eda0cb14a722a2a121b
AgustinoWhickey <wakembus98@gmail.com>
2019-10-21 21:28:15 -0700

    Revisi Sidebar

--- application/views/_template/nav.php
+++ application/views/_template/nav.php
@@ -1,9 +1,25 @@
 <div class="navbar navbar-expand-md navbar-dark">
-    <div class="navbar-brand">
+    <div class="navbar-brand" style="padding:0px;">
         <a href="index.html" class="d-inline-block">
-            <h2><?php echo SITE_NAME ?></h2>
+            <h4 style="margin-top: .625rem;"><?php echo SITE_NAME ?></h4>
         </a>
     </div>
+	
+	<div class="collapse navbar-collapse text-right" id="navbar-mobile">
+		<span class="ml-md-6 mr-md-auto"></span>
+		<ul class="navbar-nav">
+			<li class="nav-item dropdown dropdown-user">
+				<a href="#" class="navbar-nav-link d-flex align-items-center dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+					<i class="icon-reading mr-2"></i><span>Rusli Cost Control [SX_Rusli]</span>
+				</a>
+
+				<div class="dropdown-menu dropdown-menu-right">
+					<a href="#" class="dropdown-item"><i class="icon-cog5"></i> Ubah Password</a>
+					<a href="#" class="dropdown-item"><i class="icon-switch2"></i> Logout</a>
+				</div>
+			</li>
+		</ul>
+	</div>
 
     <div class="d-md-none">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-mobile">
@@ -12,29 +28,5 @@
         <button class="navbar-toggler sidebar-mobile-main-toggle" type="button">
             <i class="icon-paragraph-justify3"></i>
         </button>
-    </div>
-
-    <div class="collapse navbar-collapse" id="navbar-mobile">
-        <span class="navbar-text ml-md-3 mr-md-auto">	
-        </span>
-        <?php if(isset($this->session->userdata['user'])){ ?>
-            <ul class="navbar-nav">
-                <li class="nav-item dropdown dropdown-user">
-                    <a href="#" class="navbar-nav-link dropdown-toggle" data-toggle="dropdown">
-                        <img src="../global_assets/images/placeholders/placeholder.jpg" class="rounded-circle" alt="">
-                        <span>Alam Sutra Manager (SX Alam)</span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a href="#" class="dropdown-item"><i class="icon-user-plus"></i> My profile</a>
-                        <a href="#" class="dropdown-item"><i class="icon-coins"></i> My balance</a>
-                        <a href="#" class="dropdown-item"><i class="icon-comment-discussion"></i> Messages <span class="badge badge-pill bg-blue ml-auto">58</span></a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item"><i class="icon-cog5"></i> Account settings</a>
-                        <a href="#" class="dropdown-item"><i class="icon-switch2"></i> Logout</a>
-                    </div>
-                </li>
-            </ul>
-        <?php }else{}?>
     </div>
 </div>
--- application/views/_template/sidebar.php
+++ application/views/_template/sidebar.php
@@ -18,6 +18,20 @@
 	<div class="sidebar-content">
 
 		<!-- Main navigation -->
+		<div class="sidebar-user">
+			<div class="card-body">
+				<div class="media">
+
+					<div class="media-body">
+						<div class="media-title font-weight-semibold">Bintaro</div>
+						<div class="font-size-md opacity-50">
+							Bintaro / WDFGBNST <a href="#" style="color:white;"><i class="icon-git-compare"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
 		<div class="card card-sidebar-mobile">
 			<ul class="nav nav-sidebar" data-nav-type="accordion" id="nav">
 
@@ -43,12 +57,7 @@
                 				<li class="nav-item"><a href="<?php echo base_url('/transaksi1/transferininteroutlet');?>" class="nav-link">Transfer In Inter Outlet</a></li>
 								<li class="nav-item"><a href="<?php echo base_url('/transaksi1/purchase_request');?>" class="nav-link">Purchase Request (PR)</a></li>
 								<li class="nav-item"><a href="<?php echo site_url('/transaksi1/returnout');?>" class="nav-link">Retur Out</a></li>
-<<<<<<< HEAD
-								<li class="nav-item"><a href="../seed/sidebar_right.html" class="nav-link">Retur In</a></li>
-=======
-
 								<li class="nav-item"><a href="<?php echo site_url('/transaksi1/returnin');?>" class="nav-link">Retur In</a></li>
->>>>>>> 4394ee0651f446f1b410bb2f314affa0ad9931b6
 								<li class="nav-item"><a href="<?php echo site_url('/transaksi1/goodissue');?>" class="nav-link">Good Issue</a></li>
 								<li class="nav-item"><a href="<?php echo site_url('/transaksi1/grnopo');?>" class="nav-link">Good Receipt Non PO</a></li>
 							</ul>
