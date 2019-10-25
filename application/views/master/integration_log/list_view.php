566eb9a96b5aba060b68e50665a40fda0e4215c5
AgustinoWhickey <wakembus98@gmail.com>
2019-10-25 11:14:50 +0700

    Show Integration Log

--- application/views/master/integration_log/list_view.php
+++ application/views/master/integration_log/list_view.php
@@ -12,13 +12,13 @@
 				<div class="content">
                     <div class="card">
                         <div class="card-header">
-                            <legend class="font-weight-semibold"><i class="icon-list mr-2"></i>List Manajemen Pengguna</legend>
+                            <legend class="font-weight-semibold"><i class="icon-list mr-2"></i>List Integration Log</legend>
                         </div>
                         <div class="card-body">
                             <table id="table-manajemen" class="table table-striped " style="width:100%">
                                 <thead>
                                     <tr>
-                                        <th style="text-align: left">*</th>
+                                        <th></th>
                                         <th>Modul</th>
                                         <th>Message</th>
                                         <th>Error Time</th>
@@ -27,6 +27,51 @@
                                     </tr>
                                 </thead>
                                 <tbody>
+								<?php 
+									$i = 1;
+									if(isset($data)){
+										foreach($data as $_data){ 
+											$mod =$_data->modul;
+											if ($mod=='Good Issue from Production' || $mod=='Good Receipt Produksi'){
+												$modul='produksi';
+											}else if ($mod=='Purchase Request'){
+												$modul='pr';
+											}else if ($mod=='Store Room Request'){
+												$modul='stdstock';
+											}else if ($mod=='GR PO From Vendor'){
+												$modul='grpo';
+											}else if ($mod=='Good Issue'){
+												$modul='issue';
+											}else if ($mod=='Good Receipt Whole to Slice'||$mod=='Good Issue from Whole Outlet'){
+												$modul='twtsnew';
+											}else if ($mod=='Transfer Out Inter Outlet'){
+												$modul='gistonew_out';
+											}else if ($mod=='Retur'){
+												$modul='gisto_dept';
+											}else if ($mod=='Purchase Request'){
+												$modul='pr';
+											}else if ($mod=='Store Room Request'){
+												$modul='stdstock';
+											}else if ($mod=='GR From Sentul'){
+												$modul='grpodlv';
+											}else if ($mod=='Stock Counting Opname'){
+												$modul='opname';
+											}else if ($mod=='Transfer In Inter Outlet'){
+												$modul='grsto';
+											}
+								?> 
+									<tr>
+										<td><?php echo $i++; ?></td>
+										<td><?php echo $_data->modul; ?></td>
+										<td><?php echo $_data->message; ?></td>
+										<td><?php echo $_data->time_error; ?></td>
+										<td><?php echo $_data->id_trans; ?></td>
+										<td><a href='<?php echo site_url('master/'.$modul.'/edit/'.$_data->id_trans)?>' ><i class='icon-file-plus2' title="Edit"></i></a></td>
+									</tr>
+								<?php
+										}
+									}
+								?>
                                 </tbody>
                             </table>
                         </div>
@@ -39,29 +84,8 @@
         <script>
             $(document).ready(function(){
                 $('#table-manajemen').DataTable({
-                    "ordering":false,  "paging": true, "searching":true,
-                    "ajax": {
-                        "url":"<?php echo site_url('master/integration/showAllData');?>",
-                        "type":"POST"
-                    },
-                    "columns": [
-                        {"data":"no"},
-                        {"data":"modul"},
-                        {"data":"message"},
-                        {"data":"error_time"},
-                        {"data":"trans_id"},
-                        {"data":"no","className":"dt-center", render:function(data, type, row, meta){
-                                rr = `<a href='<?php echo site_url('master/integration/edit')?>' ><i class='icon-file-plus2' title="Edit"></i></a>&nbsp;`;
-                                return rr;
-                            }
-                        }
-                    ]
+                    "ordering":false,  "paging": true, "searching":true
                 });
-
-                deleteConfirm = (url)=>{
-                    $('#btn-delete').attr('href', url);
-	                $('#deleteModal').modal();
-                }
             });
         </script>
 	</body>
