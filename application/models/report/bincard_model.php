534443ce6fd31c06cdab0b088d328ecc53378c24
unknown <mariobross>
2020-02-10 11:17:37 +0700

    report modul

---
+++ application/models/report/bincard_model.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Bincard_model extends CI_Model{
+
+    function __construct(){
+        parent::__construct(); 
+    }
+
+    function warehouse(){
+        $notIn = array('01','02','03','IC_DShip');
+        $SAP_MSI = $this->load->database('SAP_MSI', TRUE);
+        $SAP_MSI->select('WhsCode,WhsName');
+        $SAP_MSI->from('OWHS');
+        $SAP_MSI->where('LEFT(WhsCode,2)!=','T.');
+        $SAP_MSI->where_not_in('WhsCode',$notIn);
+
+        $query = $SAP_MSI->get();
+        $ret = $query->result_array();
+        return $ret;
+    }
+
+    function item_group(){
+        $SAP_MSI = $this->load->database('SAP_MSI', TRUE);
+        $SAP_MSI->select('ItmsGrpNam');
+        $SAP_MSI->from('OITB');
+
+        $query = $SAP_MSI->get();
+        $ret = $query->result_array();
+        return $ret;
+    }
+
+}
